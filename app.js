fcce6ce2fbb43695586fc7658b674d3ee5210902
romanjhyltsou <romanjhyltsou@gmail.com>
2022-10-31 22:26:50 +0300

    refactor:Advanced Rule '1.2 Media Alternative'

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="button delete-button";
     deleteButtonImg.classList.add("delete-image");
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt="delete";
     deleteButton.appendChild(deleteButtonImg);
 
 
